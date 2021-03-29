91ef581862b03a79d7da062531e0a081189942a8
gudk0v@outlook.com <414HideruSirefu>
2021-03-29 11:32:30 +0700

    Converter liters to gallons

---
+++ app/src/main/java/com/example/my_converter/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.my_converter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText editText;
+    private TextView textView;
+    double liters;
+    double gallons;
+
+    public void onButtonClick(View v) {
+        liters = Double.parseDouble(editText.getText().toString());
+        gallons = liters*0.264172;
+        textView.setText(String.valueOf(gallons));
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        editText = findViewById(R.id.editText);
+        textView = findViewById(R.id.textView);
+
+    }
+
+}
